3aa6b733298aa11c4dd6b50c8105b99148b9b176
Зибницкий Никита Владимирович <113308228+MrKustic@users.noreply.github.com>
2026-03-05 01:39:32 +0300

    Add norm_first parameter to transformer layers

--- my_transformer.py
+++ my_transformer.py
@@ -97,8 +97,9 @@ class MultiHeadAttention(nn.Module):
 
 
 class EncoderTransformerLayer(nn.Module):
-    def __init__(self, embed_size=256, hidden_size=256, n_heads=8, dropout=0.5):
+    def __init__(self, embed_size=256, hidden_size=256, n_heads=8, dropout=0.5, norm_first=False):
         super().__init__()
+        self.norm_first = norm_first
 
         self.attention = MultiHeadAttention(embed_size, n_heads, dropout)
 
@@ -119,24 +120,32 @@ class EncoderTransformerLayer(nn.Module):
         param attention_mask: (batch_size, length, length)
         return: (batch_size, length, embeds_size)
         '''
-        attention = self.attention(embeds, embeds, embeds, attention_mask)
+        if not self.norm_first:
+            attention = self.attention(embeds, embeds, embeds, attention_mask)
 
-        norm_attention = self.layer_norm1(attention + embeds)
+            norm_attention = self.layer_norm1(attention + embeds)
 
-        output = self.feedforward(norm_attention)
-        output = self.layer_norm2(output + norm_attention)
+            output = self.feedforward(norm_attention)
+            output = self.layer_norm2(output + norm_attention)
+
+            return output
+        
+        norm_embeds = self.layer_norm1(embeds)
+        attention = self.attention(norm_embeds, norm_embeds, norm_embeds, attention_mask) + embeds
+
+        output = self.feedforward(self.layer_norm2(attention)) + attention
 
         return output
 
 
 class EncoderTransformer(nn.Module):
-    def __init__(self, max_length=128, embed_size=256, feedforward_hidden_size=256, n_heads=8, n_layers=2, dropout=0.5):
+    def __init__(self, max_length=128, embed_size=256, feedforward_hidden_size=256, n_heads=8, n_layers=2, dropout=0.5, norm_first=False):
         super().__init__()
 
         self.pe = PositionalEncoding(max_length, embed_size, dropout)
 
         self.transformer_layers = nn.ModuleList([
-            EncoderTransformerLayer(embed_size, feedforward_hidden_size, n_heads, dropout)
+            EncoderTransformerLayer(embed_size, feedforward_hidden_size, n_heads, dropout, norm_first)
             for _ in range(n_layers)
             ])
 
@@ -158,9 +167,11 @@ class EncoderTransformer(nn.Module):
 
         return output
 
+
 class DecoderTransformerLayer(nn.Module):
-    def __init__(self, embed_size=256, feedforward_hidden_size=256, n_heads=8, dropout=0.5):
+    def __init__(self, embed_size=256, feedforward_hidden_size=256, n_heads=8, dropout=0.5, norm_first=False):
         super().__init__()
+        self.norm_first = norm_first
 
         self.masked_attention = MultiHeadAttention(embed_size, n_heads, dropout)
         self.cross_attention = MultiHeadAttention(embed_size, n_heads, dropout)
@@ -168,6 +179,7 @@ class DecoderTransformerLayer(nn.Module):
         self.layer_norm1 = nn.LayerNorm(embed_size)
         self.layer_norm2 = nn.LayerNorm(embed_size)
         self.layer_norm3 = nn.LayerNorm(embed_size)
+        self.layer_norm4 = nn.LayerNorm(embed_size)
 
         self.feedforward = nn.Sequential(
             nn.Linear(embed_size, feedforward_hidden_size),
@@ -197,26 +209,38 @@ class DecoderTransformerLayer(nn.Module):
 
         mask = self.create_causal_mask_(batch_size, length)
 
-        masked_attention_output = self.masked_attention(embeds, embeds, embeds, mask)
-        norm_masked_output = self.layer_norm1(masked_attention_output + embeds)
+        if not self.norm_first:
+            masked_attention_output = self.masked_attention(embeds, embeds, embeds, mask)
+            norm_masked_output = self.layer_norm1(masked_attention_output + embeds)
 
-        cross_attention_output = self.cross_attention(norm_masked_output, encoder_output, encoder_output, attention_mask)
-        norm_cross_output = self.layer_norm2(cross_attention_output + norm_masked_output)
+            cross_attention_output = self.cross_attention(norm_masked_output, encoder_output, encoder_output, attention_mask)
+            norm_cross_output = self.layer_norm2(cross_attention_output + norm_masked_output)
 
-        feedforward_output = self.feedforward(norm_cross_output)
-        output = self.layer_norm3(feedforward_output + norm_cross_output)
+            feedforward_output = self.feedforward(norm_cross_output)
+            output = self.layer_norm3(feedforward_output + norm_cross_output)
+
+            return output
+
+        norm_embeds = self.layer_norm1(embeds)
+        masked_attention_output = self.masked_attention(norm_embeds, norm_embeds, norm_embeds, mask) + embeds
+
+        norm_masked_attention = self.layer_norm2(masked_attention_output)
+        cross_attention_output = self.cross_attention(norm_masked_attention, encoder_output, encoder_output, attention_mask) + masked_attention_output
+
+        feedforward_output = self.feedforward(self.layer_norm3(cross_attention_output)) + cross_attention_output
+        output = self.layer_norm4(feedforward_output)
 
         return output
 
 
 class DecoderTransformer(nn.Module):
-    def __init__(self, max_length=128, embed_size=256, feedforward_hidden_size=256, n_heads=8, n_layers=2, dropout=0.5):
+    def __init__(self, max_length=128, embed_size=256, feedforward_hidden_size=256, n_heads=8, n_layers=2, dropout=0.5, norm_first=False):
         super().__init__()
 
         self.pe = PositionalEncoding(max_length, embed_size, dropout)
 
         self.transformer_layers = nn.ModuleList([
-            DecoderTransformerLayer(embed_size, feedforward_hidden_size, n_heads, dropout)
+            DecoderTransformerLayer(embed_size, feedforward_hidden_size, n_heads, dropout, norm_first)
             for _ in range(n_layers)
             ])
 
@@ -240,7 +264,7 @@ class DecoderTransformer(nn.Module):
 
 
 class EncoderDecoderTransformer(nn.Module):
-    def __init__(self, dataset, embed_size=256, feedforward_hidden_size=256, n_heads=8, n_layers=2, dropout=0.5):
+    def __init__(self, dataset, embed_size=256, feedforward_hidden_size=256, n_heads=8, n_layers=2, dropout=0.5, norm_first=False):
         super().__init__()
 
         vocab_sizes = dataset.vocab_sizes
@@ -250,8 +274,8 @@ class EncoderDecoderTransformer(nn.Module):
         self.encoder_embedding = nn.Embedding(vocab_sizes[0], embed_size)
         self.decoder_embedding = nn.Embedding(vocab_sizes[1], embed_size)
 
-        self.encoder = EncoderTransformer(self.max_length, embed_size, feedforward_hidden_size, n_heads, n_layers, dropout)
-        self.decoder = DecoderTransformer(self.max_length, embed_size, feedforward_hidden_size, n_heads, n_layers, dropout)
+        self.encoder = EncoderTransformer(self.max_length, embed_size, feedforward_hidden_size, n_heads, n_layers, dropout, norm_first)
+        self.decoder = DecoderTransformer(self.max_length, embed_size, feedforward_hidden_size, n_heads, n_layers, dropout, norm_first)
 
         self.linear = nn.Linear(embed_size, vocab_sizes[1])
 
@@ -271,43 +295,6 @@ class EncoderDecoderTransformer(nn.Module):
         logits = self.linear(decoder_output)
 
         return logits
-
-    # @torch.inference_mode()
-    # def inference(self, indices: torch.Tensor, lengths: torch.Tensor, temp: float = 1.) -> List[str]:
-    #     '''
-    #     param indices -- batch of padded tokenized input sequences (batch_size, length)
-    #     params lengths -- real lengths of sequences (batch_size,)
-    #     return: batch of generated strings
-    #     '''
-
-    #     self.eval()
-    #     device = next(self.parameters()).device
-
-    #     input_embeddings = self.encoder_embedding(indices)
-    #     encoder_output = self.encoder(input_embeddings, lengths)
-
-    #     batch_size = indices.shape[0]
-    #     tokens = torch.tensor([self.dataset.bos_id] * batch_size, device=device, dtype=torch.int32)[:, None]
-    #     mask_eos = torch.zeros((batch_size,), device=device, dtype=torch.bool)
-    #     token_embeddings = self.decoder_embedding(tokens) # (batch_size, 1, embed_size)
-
-    #     for i in range(self.max_length - 1):
-    #         decoder_output = self.decoder(token_embeddings, encoder_output, lengths)
-
-    #         logits = self.linear(decoder_output)
-    #         new_tokens = Categorical(logits=logits[:, -1, :] / temp).sample() # (batch_size,)
-    #         new_tokens = torch.where(mask_eos, self.dataset.pad_id, new_tokens)
-    #         mask_eos = mask_eos | (new_tokens == self.dataset.eos_id)
-    #         tokens = torch.cat([tokens, new_tokens[:, None]], dim=1)
-    #         new_embeddings = self.decoder_embedding(new_tokens[:, None])
-    #         token_embeddings = torch.cat([token_embeddings, new_embeddings], dim=1) # (batch_size, length, embed_size)
-
-    #         if mask_eos.sum() == batch_size:
-    #             break
-        
-    #     generated = self.dataset.ids2text(tokens, lang='en')
-
-    #     return generated
 
     @torch.inference_mode()
     def inference(self, indices: torch.Tensor, lengths: torch.Tensor, temp: float = 1., num_beam_paths: int=None) -> List[str]:
@@ -362,7 +349,7 @@ class EncoderDecoderTransformer(nn.Module):
             decoder_output = self.decoder(token_embeddings, encoder_output, lengths)
 
             logits = self.linear(decoder_output)
-            logits = -log_softmax(logits[:, -1, :], dim=-1)
+            logits = -log_softmax(logits[:, -1, :] / temp, dim=-1)
 
             new_tokens = torch.topk(logits, k=num_beam_paths, dim=1, largest=False)[1]
             new_probs = torch.gather(logits, dim=1, index=new_tokens) # (batch_size, num_beam_paths) or (batch_size * num_bram_paths, num_beam_paths)
